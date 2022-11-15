2b986e7e8bb607b943bfb4c2358cf634945a92a2
Federico Bobbo <federico.bobbo@gmail.com>
2022-11-15 11:25:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
-function preload() {
-	// put preload code here
-}
+let myColor=10;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+	createCanvas(windowWidth, windowHeight)
+	console.log("Move me babeeee!")
+	// by defaults equals to 0.5
+	setMoveThreshold(2);
+	textSize(20)
+	textAlign(CENTER)
 }
 
 function draw() {
-	// put drawing code here
+	background(myColor)
+	text('Move to read me better', width/2, height/2)
+}
+
+function deviceMoved() {
+  	myColor+=0.3;
+	if (myColor>255) myColor=10;
+}	
+
+// request permissions on iOS
+function touchEnded(event) {
+	if(DeviceOrientationEvent && DeviceOrientationEvent.requestPermission) {
+		DeviceOrientationEvent.requestPermission()
+	}
 }
